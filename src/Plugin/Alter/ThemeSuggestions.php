7be4bc9a600de2efa92e7596d5cd143de7f5801c
RabiaSajjad <rabiasajjad.dev@gmail.com>
2019-05-26 19:05:11 -0400

    voting, blocks, forms

---
+++ src/Plugin/Alter/ThemeSuggestions.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\gcweb\Plugin\Alter;
+
+use Drupal\bootstrap\Plugin\Alter\ThemeSuggestions as BootstrapThemeSuggestions;
+use Drupal\bootstrap\Utility\Variables;
+
+/**
+ * Implements hook_theme_suggestions_alter().
+ *
+ * @ingroup plugins_alter
+ *
+ * @BootstrapAlter("theme_suggestions")
+ */
+class ThemeSuggestions extends BootstrapThemeSuggestions {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alter(&$suggestions, &$context1 = NULL, &$hook = NULL) {
+    $variables = Variables::create($context1);
+
+    switch ($hook) {
+      case 'block':
+        if (isset($variables['elements']['content']['#block_content'])) {
+          $bundle = $variables['elements']['content']['#block_content']->bundle();
+          $suggestions[] = 'block__' . $bundle;
+        }
+        break;
+
+      case 'form':
+        $form_id = $variables['element']['#id'];
+        $suggestions[] = 'form__' . str_replace('-','_',$form_id);
+        break;
+
+      case 'page':
+        $node = \Drupal::routeMatch()->getParameter('node');
+        if (is_object($node)) {
+          $suggestions[] = 'page__' . $node->bundle();
+        }
+        break;
+    }
+
+    parent::alter($suggestions, $context1, $hook);
+  }
+
+}
---
+++ src/Plugin/Form/CatalogSearchBlockForm.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @file
+ * Contains Drupal\gcweb\Plugin\Form\SearchBlockForm.
+ */
+
+namespace Drupal\gcweb\Plugin\Form;
+
+use Drupal\bootstrap\Bootstrap;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Routing\TrustedRedirectResponse;
+
+class CatalogSearchBlockForm extends FormBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'catalog_search_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form['#attributes']['class'][] = 'form-inline';
+    $form['#method'] = 'post';
+
+    $form['query'] = [
+      '#id' => 'query',
+      '#type' => 'textfield',
+      '#title' => '',
+      '#size' => 60,
+      '#maxlength' => 256,
+      '#default_value' => '',
+      '#placeholder' => '',
+    ];
+
+    $form['url'] = [
+      '#id' => 'url',
+      '#type' => 'hidden',
+    ];
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Find'),
+      '#id' => 'search_submit',
+      '#button_type' => 'primary',
+      '#icon' => Bootstrap::glyphicon('search'),
+//      '#prefix' => '<span class="input-group-btn">',
+//      '#suffix' => '</span>',
+    ];
+
+    return $form;
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // redirect to the url link
+    $url = $form_state->getValue('url') . $form_state->getValue('query');
+    $response = new TrustedRedirectResponse($url);
+    $response->send();
+  }
+}

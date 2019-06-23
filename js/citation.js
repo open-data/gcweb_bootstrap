92ce872ab87962e95971c3e37b3f26e83c0968c8
RabiaSajjad <rabiasajjad.dev@gmail.com>
2019-06-23 19:56:40 -0400

    slight update to citation structure and format

--- js/citation.js
+++ js/citation.js
@@ -1,33 +1,36 @@
 function selectText(containerid) {
-    if (document.selection) { // IE
-        document.selection.empty();
-        var range = document.body.createTextRange();
-        range.moveToElementText(document.getElementById(containerid));
-        range.select();
-    } else if (window.getSelection) {
-        window.getSelection().removeAllRanges();
-        var range = document.createRange();
-        range.selectNode(document.getElementById(containerid));
-        window.getSelection().removeAllRanges();
-        window.getSelection().addRange(range);
-    }
+  if (document.selection) { // IE
+    document.selection.empty();
+    var range = document.body.createTextRange();
+    range.moveToElementText(document.getElementById(containerid));
+    range.select();
+  }
+  else if (window.getSelection) {
+    window.getSelection().removeAllRanges();
+    var range = document.createRange();
+    range.selectNode(document.getElementById(containerid));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+  }
 }
+
 function copyText(containerid) {
-    if (document.selection) {
-        document.selection.empty();
-        var range = document.body.createTextRange();
-        range.moveToElementText(document.getElementById(containerid));
-        range.select().createTextRange();
-        document.execCommand("copy");
+  if (document.selection) {
+    document.selection.empty();
+    var range = document.body.createTextRange();
+    range.moveToElementText(document.getElementById(containerid));
+    range.select().createTextRange();
+    document.execCommand("copy");
+  }
+  else if (window.getSelection) {
+    document.getElementById('selectableAPACopied').innerHTML="";
+    document.getElementById('selectableMLACopied').innerHTML="";
 
-    } else if (window.getSelection) {
-        document.getElementById('selectableAPACopied').innerHTML="";
-        document.getElementById('selectableMLACopied').innerHTML="";
-
-        window.getSelection().removeAllRanges();
-        var range = document.createRange();
-        range.selectNode(document.getElementById(containerid));
-        window.getSelection().addRange(range);
-        document.execCommand("copy");
-        document.getElementById(containerid+"Copied").innerHTML="copied!";
-    }}
+    window.getSelection().removeAllRanges();
+    var range = document.createRange();
+    range.selectNode(document.getElementById(containerid));
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    document.getElementById(containerid+"Copied").innerHTML="copied!";
+  }
+}

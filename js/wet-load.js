2997a07a7c7537b9e1c37f3cae8cfb9bd8e1b273
RabiaSajjad <rabiasajjad.dev@gmail.com>
2019-10-03 10:53:27 -0400

    WET: upgrade to using CDTS

---
+++ js/wet-load.js
@@ -0,0 +1,41 @@
+/**
+ * @file
+ * Load WET-BOEW CDTS closure template scripts for
+ * - Head
+ * - Top
+ * - PreFooter
+ * - Footer
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.WETLoad = {
+    attach: function (context, settings) {
+      $(document).ready(function() {
+        // load CDTS Head reference
+        $('head').append(wet.builder.refTop({
+        }));
+
+        // load CDTS Top Reference
+        let defTop = document.getElementById("def-top");
+        defTop.outerHTML = wet.builder.top({
+          "lngLinks": false,
+          "breadcrumbs": false
+        });
+
+        // load CDTS PreFooter Reference
+        let defPreFooter = document.getElementById("def-preFooter");
+        defPreFooter.outerHTML = wet.builder.preFooter({
+          "showFeedback" : drupalSettings.feedback_link,
+          "dateModified": drupalSettings.date_modified
+        });
+
+        // load CDTS Footer refernce
+        let defFooter = document.getElementById("def-footer");
+        defFooter.outerHTML = wet.builder.footer();
+      });
+    }
+  };
+})(window.jQuery, window.Drupal, window.drupalSettings);

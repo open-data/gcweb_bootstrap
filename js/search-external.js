2e4a6eb564e0c34efd311020a27e7fadef23e312
RabiaSajjad <rabiasajjad.dev@gmail.com>
2022-02-25 10:38:08 -0500

    redirect search form to external search

--- js/search-external.js
+++ js/search-external.js
@@ -4,6 +4,8 @@
 
   $('#catalog-search-form').on("submit", function(e) {
     $("#catalog-search-form").attr('action', $('#catalog-search-form').attr('action') + $('input[name=query]').val());
+    window.location.href = $('input[name=url]').val() + $('input[name=query]').val();
+    return false;
   });
 
 })(jQuery, Drupal, drupalSettings);
